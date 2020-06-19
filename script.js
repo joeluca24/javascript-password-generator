79d918f37cb4d702fd19d6e714201db2ae6c4b7b
joeluca24 <65137591+joeluca24@users.noreply.github.com>
2020-06-19 12:27:01 -0400

    test commit

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// Ask user how long password is going to be.
+// Store users answer to how long the password is going to be.
+ var passwordLength = prompt("How long do you want youre password to be?")
+
+// Ask user if they want numbers in thier password.
+// Store user answer to if they want number in their password.
+var passwordNumber = confirm("Do you want number in youre password?")
+
+
+
+
+
+// Ask user if they want special charcters in their password.
+ var passworCharacter = confirm("Do you want special characters in youre password?")
+
+// Store user answer if they special characters in their password.
+console.log(passwordLength)
+console.log(passwordNumber)
+console.log(passworCharacter)
