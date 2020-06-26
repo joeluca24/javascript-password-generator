73e715e77ebc721e9386e4dfec3bc4701f1007a8
joeluca24 <65137591+joeluca24@users.noreply.github.com>
2020-06-26 18:51:21 -0400

    restructred new js code

--- script.js
+++ script.js
@@ -11,111 +11,86 @@ function writePassword() {
 
 
   passwordText.value = password;
-
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
-
-
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
-  function generatePassword() {
-    var possible = [];
-    var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    var passwordNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-    var specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-    var password = "";
-    var passwordLength = false;  
-
-// WHEN prompted for the length of the password
-  // THEN I choose a length of at least 8 characters and no more than 128 characters
-  // We need a prompt here and ask for the length from the user
-  // Check if the number is < 8 and > 128   
-  
-    while (passwordLength < 8 || passwordLength > 128 || passwordLength == "") { 
-      passwordLength = prompt("Please choose a valid length: Choose a length between 8 and 128")
 
 
-      //*********************4 confirms****************
-  // WHEN prompted for character types to include in the password
-  // THEN I choose lowercase, uppercase, numeric, and/or special characters
-  // We need a Confirm and ask the user if they want to use lowercase?
-  // We need a Confirm and ask the user if they want to use uppercase?
-  // We need a Confirm and ask the user if they want to use numeric?
-  // We need a Confirm and ask the user if they want to use special Characters?
- 
-  // Store users answer to how long the password is going to be.
-   var passwordLength = prompt("How long do you want your password to be?")
+function generatePassword() {
+  var possible = [];
+  // var possible = "abcdABCD0912#!@#$";
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+  var password = "";
+  var passwordLength = false;
 
- // Ask user if they want numbers in thier password.
- // Store user answer to if they want number in their password.
-  var passwordNumber = confirm("Do you want numbers in your password?")
-
-
- // Ask user if they want special charcters in their password.
- // Store user answer if they special characters in their password.
-   var passwordCharacter = confirm("Do you want special characters in your` password?")
-
-
-  var otherLowerCase = confirm("Do you want to include lowercase letters in your password?");
-  var otherUpperCase = confirm("Do you want to include uppercase letters your password?");
-  var otherNumbers = confirm("Do you want to include numbers in your password?");
-  var passwordCharacter = confirm("Do you want to include special characters in your password?");
-
-  / if lowercase = true 
-  if (otherLowerCase === true) {
-    // than we can add into the empty array 
-    possible = possible.concat(lowerCase);
-    
-  }
-  if (otherUpperCase === true) {
-    // add to array 
-    possible = possible.concat(upperCase);
-   
-  }
-  if (otherNumbers === true) {
-    // add to array 
-    possible = possible.concat(numbers);
-   
-  }
-  if (passwordCharacter === true) {
-    // add to array  
-    possible = possible.concat(specChar);
-    console.log(possible);
-  }
-​
-// console.log(passwordLength)
-// console.log(passwordNumber)
-// console.log(passworCharacter)
-
-    
-  // Store users answer to how long the password is going to be.
-  var passwordLength = prompt("How long do you want youre password to be?")
-
-  // Ask user if they want numbers in thier password.
-  // Store user answer to if they want number in their password.
-  var passwordNumber = confirm("Do you want numbers in your password?")
-
-
-  // Ask user if they want special charcters in their password.
-  // Store user answer if they special characters in their password.
-  var passwordCharacter = confirm("Do you want special characters in youre password?")
-  
-
-  }
-  // WHEN I click the button to generate a password
-  //??????? what series
-  // THEN I am presented with a series of prompts for password criteria
-  // WHEN prompted for password criteria
-  // THEN I select which criteria to include in the password
-
-  // ************* 1 prompt ***********
-  //************** Need validation*******
   // WHEN prompted for the length of the password
   // THEN I choose a length of at least 8 characters and no more than 128 characters
   // We need a prompt here and ask for the length from the user
-  // Check if the number is < 8 and > 128
+  // Check if the number is < 8 and > 128   
 
+  while (passwordLength < 8 || passwordLength > 128 || passwordLength + "" === 'NaN') {
+    // "" < 8
+    // 0 < 8
+    passwordLength = prompt("Please choose a valid length: Choose a length between 8 and 128");// return string
+    passwordLength = parseInt(passwordLength);
+  }
+
+  //*********************4 confirms****************
+  // WHEN prompted for character types to include in the password
+  // THEN I choose lowercase, uppercase, numeric, and/or special characters
+  // We need a Confirm and ask the user if they want to use lowercase?
+  var otherLowerCase = confirm("Do you want to include lowercase letters in your password?");
+  // We need a Confirm and ask the user if they want to use uppercase?
+  var otherUpperCase = confirm("Do you want to include uppercase letters your password?");
+  // We need a Confirm and ask the user if they want to use numeric?
+  var otherNumbers = confirm("Do you want to include numbers in your password?");
+  // We need a Confirm and ask the user if they want to use special Characters?
+  var passwordCharacter = confirm("Do you want to include special characters in your password?");
+
+
+  //if lowercase = true 
+  if (otherLowerCase) {
+    // than we can add into the empty array 
+    //possible = possible.concat(lowerCase);
+    for (var i = 0; i < lowerCase.length; i++) {
+      console.log(lowerCase[i]);
+      possible.push(lowerCase[i]);
+    }
+
+  }
+  if (otherUpperCase) {
+    // than we can add into the empty array 
+    //possible = possible.concat(lowerCase);
+    for (var i = 0; i < upperCase.length; i++) {
+      possible.push(upperCase[i]);
+    }
+
+  }
+  if (otherNumbers) {
+    for (var i = 0; i < numbers.length; i++) {
+      console.log(numbers[i]);
+      possible.push(numbers[i]);
+    }
+  }
+  if (passwordCharacter) {
+    for (var i = 0; i < specChar.length; i++) {
+      console.log(specChar[i]);
+      possible.push(specChar[i]);
+    }
+  }
+
+
+
+
+
+
+  // // ************* 1 prompt ***********
+  // //************** Need validation*******
 
   for (var i = 0; i < passwordLength; i++) {
     var randomChar = possible[Math.floor(Math.random() * possible.length)];
@@ -123,8 +98,8 @@ function writePassword() {
     console.log(password);
   }
   return password; // return passworfd when input in completeed succesfully. 
-​
-​
+
+
 }
 
   // WHEN I answer each prompt
@@ -132,18 +107,10 @@ function writePassword() {
   // each prompt needs to be validated...
 
 
-
-  //************ need a function *******************
-  // WHEN all prompts are answered
-  // THEN a password is generated that matches the selected criteria
-  // All the information is gathered (all prompts finished getting input)
-  // All the HARD logic goes here ???????
-
-
   // WHEN the password is generated
   // THEN the password is either displayed in an alert or written to the page
   // Either do an alert with password.. .or just check to see if my variable has a password
 
 
- 
+
 
